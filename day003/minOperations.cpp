b967df8ad0c25a6af4f049843ab548c8a4a8ba7c
mikehrmantraut <baran_3430@outlook.com>
2025-04-09 21:37:27 +0300

    day3

---
+++ day003/minOperations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        int minOperations(vector<int>& nums, int k){
+            unordered_set<int>s;
+            for (int num:nums){
+                if (num < k) return -1;
+                if (num > k) s.insert(num);
+            }
+            return s.size();
+        }
+    };
+    
